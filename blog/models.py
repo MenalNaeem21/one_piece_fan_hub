cc11791f15fb76beace8e4844c6e7f163beacee5
MenalNaeem21 <menalnaeem21@gmail.com>
2025-01-31 16:45:55 +0500

    added discussions class

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,15 @@
 from django.db import models
 from datetime import datetime
+from django.contrib.auth.models import User
 # Create your models here.
 class Post(models.Model):
     title=models.CharField(max_length=100)
     body=models.CharField(max_length=100000)
     created_at=models.DateTimeField(default=datetime.now,blank=True)
-    
+
+class Discussion(models.Model):
+    title=models.CharField(max_length=300)    
+    content=models.TextField()
+    author=models.ForeignKey(User, on_delete=models.CASCADE) #user who created the post
+    created_at=models.DateField(auto_now_add=True)
+    updated_at=models.DateField(auto_now=True)
